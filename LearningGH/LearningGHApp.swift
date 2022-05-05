bfe00b2760081dd645bf6d8f0851763a38a29eed
Traian Cretu <traiancretu@yahoo.com>
2022-05-05 09:03:56 +0300

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Traian Cretu on 05.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
